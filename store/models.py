1e0a1e1e256b2e9af732b15b123b876468cc5ea3
tinoe0404 <tinoe044@gmail.com>
2025-08-03 16:39:58 +0200

    one to one relationship between 2 models

--- store/models.py
+++ store/models.py
@@ -25,6 +25,11 @@ class Customer(models.Model):
     birth_date = models.DateField(null = True)
     membership = models.CharField(max_length=1, choices = MEMBERSHIP_CHOICES, default= 'B' )
 
+class Address(models.Model):
+    street = models.CharField(max_length = 255)
+    city = models.CharField(max_length = 255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
