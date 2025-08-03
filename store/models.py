0c2373aef6187e151d828ed5aeba2536ca1986cc
tinoe0404 <tinoe044@gmail.com>
2025-08-03 17:14:34 +0200

    creating many to many relationship between models

--- store/models.py
+++ store/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Promotion(models.Model):
+    description = models.Charfiled(max_length = 255)
+    discount = models.FloatField()
+    # products
+
 
 class Collection(models.Model):
     title = models.CharField(max_length = 255)
@@ -11,6 +16,7 @@ class Product(models.Model):
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer(models.Model):
